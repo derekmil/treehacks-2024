f75a36c1c0d20b26aadd4d3ec898caabdefa9db9
sliu <48802197+lambdasc@users.noreply.github.com>
2024-02-17 15:35:42 -0800

    tree

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,17 @@
-"use client";
+import { useState } from 'react'
+import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 
-import { useEffect, useRef, useState } from "react";
-import { Button } from "../src/components/ui/button";
-import { ThemeProvider } from "../src/components/theme-provider";
-import { ModeToggle } from "../src/components/mode-toggle"; // Adjust the path as necessary
-import MonacoEditor from "./components/monacoEditor";
+import './App.css'
 
-const value = /* set from `myEditor.getModel()`: */ `function hello() {
-	alert('Hello world!');
-}`;
+function App() {
 
-export default function Home() {
-  const [theme, setTheme] = useState("light");
-  const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
-  };
+
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <main className="flex min-h-screen flex-col items-center justify-between p-32">
-        <nav className="w-full flex justify-between items-center mb-4">
-          <div className="flex">
-            <h1 className="font-bold text-8xl">AI.DE</h1>
-            <p className="pt-16 pl-5">
-              For the next generation of resume builders and math proof makers.
-            </p>
-          </div>
-          <ModeToggle />
-        </nav>
-        <div className="flex items-center justify-between">
-          <div className="flex w-full h-full rounded-md overflow-hidden">
-            {" "}
-            <MonacoEditor />
-          </div>
-
-          <div>
-            <h1>PDF RENDERERER TO BE CONTINUED</h1>
-          </div>
-        </div>
-      </main>
-    </ThemeProvider>
-  );
+    <div>
+      <Editor height="90vh" width="100vw" defaultLanguage="javascript" defaultValue="// some comment" />;
+    </div>
+  )
 }
+
+export default App
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,7 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import MonacoEditorPlugin from "vite-plugin-monaco-editor";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    MonacoEditorPlugin({
-      // Specify languages to include, and other configurations as needed
-      languages: ["typescript", "javascript"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [react()],
+})
