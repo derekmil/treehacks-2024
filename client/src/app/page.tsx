d9baef8c4dd0d01114620fd1ec4b3e266fa44a3f
Michael Kim <michaelkimhyun1113@gmail.com>
2024-02-17 12:29:29 -0800

    monaco done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import dynamic from "next/dynamic";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle"; // Adjust the path as necessary
 
 // dynamiclaly importing monaco editor for clientside rendering only w/o serverised
 const MonacoEditor = dynamic(() => import("../components/monacoEditor"), {
@@ -11,15 +16,33 @@ const value = /* set from `myEditor.getModel()`: */ `function hello() {
 }`;
 
 export default function Home() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
   return (
-    <main className="flex min-h-screen flex items-center justify-between p-24">
-      <div className="flex w-full h-full">
-        <MonacoEditor />
-      </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="flex min-h-screen flex-col items-center justify-between p-32">
+        <nav className="w-full flex justify-between items-center mb-4">
+          <div className="flex">
+            <h1 className="font-bold text-8xl">AI.DE</h1>
+            <p>
+              For the next generation of resume builders and math proof makers.
+            </p>
+          </div>
+          <ModeToggle />
+        </nav>
+        <div className="flex items-center justify-between">
+          <div className="flex w-full h-full rounded-lg overflow-hidden">
+            {" "}
+            <MonacoEditor />
+          </div>
 
-      <div>
-        <h1>PDF RENDERERER TO BE CONTINUED</h1>
-      </div>
-    </main>
+          <div>
+            <h1>PDF RENDERERER TO BE CONTINUED</h1>
+          </div>
+        </div>
+      </main>
+    </ThemeProvider>
   );
 }
--- client/src/components/monacoEditor.tsx
+++ client/src/components/monacoEditor.tsx
@@ -2,19 +2,24 @@
 
 import { useRef, useEffect } from "react";
 import * as monaco from "monaco-editor";
+import { useTheme } from "../components/theme-provider"; // Adjust the import path
 
 const MonacoEditor = () => {
   const editorRef = useRef<HTMLDivElement>(null); // Create a ref for the editor container
+  const { theme } = useTheme();
 
   useEffect(() => {
     let editor: monaco.editor.IStandaloneCodeEditor | null = null;
 
-    fetch("/public/themes/Monokai.json")
+    const themeFile =
+      theme === "dark" ? "/themes/ghDark.json" : "/themes/ghLight.json";
+    const monacoThemeName = theme === "dark" ? "ghDark" : "ghLight";
+
+    fetch(themeFile)
       .then((response) => response.json())
       .then((data) => {
-        monaco.editor.defineTheme("monokai", data);
-        monaco.editor.setTheme("monokai");
-        // More code here
+        monaco.editor.defineTheme(monacoThemeName, data);
+        monaco.editor.setTheme(monacoThemeName);
       })
       .catch((error) => {
         console.error("Failed to load the theme:", error);
@@ -31,12 +36,12 @@ const MonacoEditor = () => {
     }
 
     return () => {
-      // Dispose of the editor instance on component unmount
+      // Dispose of the editor instance on component unmount (clean up of resources type shit)
       if (editor) {
         editor.dispose();
       }
     };
-  }, []);
+  }, [theme]);
 
   return <div ref={editorRef} style={{ height: "500px", width: "100%" }} />;
 };
