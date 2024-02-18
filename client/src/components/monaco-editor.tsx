66df1a0da555946c6044d54792785d887eb5e88c
Michael Kim <michaelkimhyun1113@gmail.com>
2024-02-17 18:53:13 -0800

    automatic fill

--- client/src/components/monaco-editor.tsx
+++ client/src/components/monaco-editor.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import Editor, { useMonaco } from "@monaco-editor/react";
+import { useEffect, useCallback } from "react";
+import Editor, { useMonaco, OnMount } from "@monaco-editor/react";
 import { useTheme } from "../components/theme-provider"; // Adjust the import path
 
 declare global {
@@ -15,53 +15,267 @@ const MonacoEditor = () => {
   //FUNCTIONALITY FOR AUTO COMPLETE SUGGESTION
   // So if someone types in \resumeList or whatever we decide and click enter
 
+  //USEeffect hook for updates
+
   useEffect(() => {
     if (monaco) {
-      // Register a completion item provider for the TypeScript language
+      // Ensure 'monaco' is not null
+      //typescript temporarily, can identify my own language using the following template
+      //monaco.languages.register({ id: 'mySpecialLanguage' });
+
+      // monaco.languages.registerCompletionItemProvider('mySpecialLanguage',
+
       monaco.languages.registerCompletionItemProvider("typescript", {
-        triggerCharacters: ["/"], // Trigger suggestions on '/'
-        provideCompletionItems: function (model, position) {
-          console.log("Completion provider triggered");
+        provideCompletionItems: (model, position) => {
           const word = model.getWordUntilPosition(position);
+          //logic to get rid of the first slash and word
           const range = {
             startLineNumber: position.lineNumber,
             endLineNumber: position.lineNumber,
-            startColumn: word.startColumn,
-            endColumn: word.endColumn,
+            startColumn: word.startColumn - 1, // Adjusted to include the trigger character
+            endColumn: position.column,
           };
 
-          if (word.word.startsWith("/")) {
-            return {
-              suggestions: [
-                {
-                  label: "/list",
-                  kind: monaco.languages.CompletionItemKind.Snippet,
-                  documentation: "Insert a LaTeX list template",
-                  insertText: [
-                    "\\begin{itemize}",
-                    "  \\item First item",
-                    "  \\item Second item",
-                    "\\end{itemize}",
-                  ].join("\n"),
-                  range: range,
-                },
-                // Additional suggestions can be added here
-              ],
-            };
-          }
+          //list of commands
+          var suggestions = [
+            {
+              label: "/resumeList",
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: [
+                "% === BEGIN resumeList SECTION ===",
+                "\\begin{itemize}",
+                "  \\item ${1:First item}",
+                "  \\item ${2:Second item}",
+                "\\end{itemize}",
+                "% === END resumeList SECTION ===",
+              ].join("\n"),
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "/unionsetproof",
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: [
+                "\\${First set}",
+                "  \\${union symbol}",
+                "  \\${secondset}",
+              ].join("\n"),
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "DeMorgan's Law",
+              kind: monaco.languages.CompletionItemKind.Function,
+              insertText: [
+                "% Applying DeMorgan's Law",
+                "% NOT (A OR B) is equivalent to NOT A AND NOT B",
+                "% NOT (A AND B) is equivalent to NOT A OR NOT B",
+                "\\overline{${1:A} \\cup ${2:B}} = \\overline{${1:A}} \\cap \\overline{${2:B}}",
+                "\\overline{${1:A} \\cap ${2:B}} = \\overline{${1:A}} \\cup \\overline{${2:B}}",
+                "% Replace A and B with your specific variables",
+              ].join("\n"),
+              detail: "Insert DeMorgan's Law with placeholders for A and B",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "not op",
+              kind: monaco.languages.CompletionItemKind.Operator,
+              insertText: "\\neg",
+              detail: "Insert Not Operator",
+            },
+            {
+              label: "or op",
+              kind: monaco.languages.CompletionItemKind.Operator,
+              insertText: "\\lor",
+              detail: "Insert Or Operator",
+            },
+            {
+              label: "and op",
+              kind: monaco.languages.CompletionItemKind.Operator,
+              insertText: "\\land",
+              detail: "Insert And Operator",
+            },
+            {
+              label: "sum op",
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: "\\sum_{i=${1:1}}^{${2:n}} ${3:expr}",
+              detail: "Insert Summation",
+            },
+            {
+              label: "uni",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\cup",
+              detail: "Insert Union of Sets",
+            },
+            {
+              label: "intersec",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\cap",
+              detail: "Insert Intersection of Sets",
+            },
 
-          return { suggestions: [] };
+            {
+              label: "curly",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\{ $1 \\}",
+              detail: "Insert Curly Braces",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "square",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\[ $1 \\]",
+              detail: "Insert Square Braces",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "angle",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\langle $1 \\rangle",
+              detail: "Insert Angle Braces",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "bar",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\bar{$1}",
+              detail: "Insert Bar Over Character",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+
+            {
+              label: "cup",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\bigcup_{$1}^{$2}",
+              detail: "Insert Big Union",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "cap",
+              kind: monaco.languages.CompletionItemKind.Method,
+              insertText: "\\bigcap_{$1}^{$2}",
+              detail: "Insert Big Intersection",
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+
+            {
+              label: "/resumeHeader",
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: [
+                "% === BEGIN resumeHeader SECTION ===",
+                "\\begin{center}",
+                "    \\textbf{\\Huge \\scshape ${1:Name}} \\\\ \\vspace{1pt}", // ${1:Name} as a fillable
+                "    \\small \\faIcon{envelope} ${2:email@example.com} $|$ \\faIcon{globe} \\href{${3:https://yourwebsite.com/}}{\\underline{Personal Website}} $|$ \\faIcon{youtube} \\href{${4:https://www.youtube.com}}{\\underline{Self Intro Video}} $|$",
+                "    \\faIcon{linkedin} \\href{${5:https://www.linkedin.com/in/yourprofile/}}{\\underline{LinkedIn}}$|$",
+                "    \\faIcon{github}",
+                "    \\href{${6:https://github.com/yourusername}}{\\underline{Github}}",
+                "\\end{center}",
+                "% === END resumeHeader SECTION ===",
+              ].join("\n"),
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: "/resumeTemplate", // Trigger label
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: [
+                // Heading
+                "% === HEADING SECTION ===",
+                "\\begin{center}",
+                "    \\textbf{\\Huge \\scshape ${1:Your Name}} \\\\ \\vspace{1pt}",
+                "    \\small \\faIcon{envelope} ${2:your_email@example.com} $|$ \\faIcon{globe} \\href{${3:https://yourwebsite.com/}}{\\underline{Personal Website}} $|$ \\faIcon{youtube} \\href{${4:https://www.youtube.com}}{\\underline{Self Intro Video}} $|$",
+                "    \\faIcon{linkedin} \\href{${5:https://www.linkedin.com/in/yourprofile/}}{\\underline{LinkedIn}}$|$",
+                "    \\faIcon{github} \\href{${6:https://github.com/yourusername}}{\\underline{Github}}",
+                "\\end{center}",
+                "% === END HEADING SECTION ===",
+
+                // Education
+                "% === EDUCATION SECTION ===",
+                "\\section{Education}",
+                "  \\resumeSubHeadingListStart",
+                "    \\resumeSubheading",
+                "      {${7:University Name}}{${8:City, State}}",
+                "      {\\emph{${9:Degree} in ${10:Major}}}{\\emph{${11:Graduation Month, Year}}}",
+                "  \\resumeSubHeadingListEnd",
+                "% === END EDUCATION SECTION ===",
+
+                // Experience
+                "% === EXPERIENCE SECTION ===",
+                "\\section{Experience}",
+                "  \\resumeSubHeadingListStart",
+                "    \\resumeSubheading",
+                "      {${12:Company Name}}{${13:Location}}",
+                "      {\\emph{${14:Job Title}}}{\\emph{${15:Start Date – End Date}}}",
+                "      \\resumeItemListStart",
+                "        \\resumeItem{${16:Description of your responsibility or achievement}}",
+                "      \\resumeItemListEnd",
+                "  \\resumeSubHeadingListEnd",
+                "% === END EXPERIENCE SECTION ===",
+
+                // Projects
+                "% === PROJECTS SECTION ===",
+                "\\section{Projects}",
+                "  \\resumeSubHeadingListStart",
+                "    \\resumeProjectHeading",
+                "          {\\textbf{${17:Project Name}} $|$ \\emph{${18:Technologies Used}}}{${19:Date}}",
+                "          \\resumeItemListStart",
+                "            \\resumeItem{${20:Project description and achievements}}",
+                "          \\resumeItemListEnd",
+                "  \\resumeSubHeadingListEnd",
+                "% === END PROJECTS SECTION ===",
+
+                // Additional Information
+                "% === ADDITIONAL INFORMATION SECTION ===",
+                "\\section{Additional}",
+                " \\begin{itemize}[leftmargin=0.15in, label={}]",
+                "    \\small{\\item{",
+                "       \\resumeItemListStart",
+                "            \\resumeItem{${21:Interesting fact or hobby}}",
+                "       \\resumeItemListEnd",
+                "    }}",
+                " \\end{itemize}",
+                "% === END ADDITIONAL INFORMATION SECTION ===",
+              ].join("\n"), // Join the array elements into a single string with new lines
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+          ];
+          //map function to get rid of the length fo the concatenated string
+          return {
+            suggestions: suggestions.map((suggestion) => ({
+              ...suggestion,
+              range,
+            })),
+          };
         },
       });
+
+      const container = document.getElementById("container");
+      if (container) {
+        // Ensure 'container' is not null
+        monaco.editor.create(container, {
+          language: "typescript",
+          // Other editor options...
+        });
+      }
     }
   }, [monaco]);
+
+  // Create or configure the editor instance
 
   return (
     <Editor
       height="500px"
       defaultLanguage="typescript"
       defaultValue="// some comment"
-      theme={theme === "dark" ? "vs-dark" : "light"} // Use monaco's built-in themes as fallback
+      theme={theme === "dark" ? "vs-dark" : "light"}
       beforeMount={(monaco) => {
         // able to  can extend monaco functionalities if needed before mounting the editor
         // For example, registering completion item providers
