7aa717f3fc963310ab821325291fa219b01252f4
Derek Miller <derekmil@umich.edu>
2024-02-18 02:28:19 -0800

    holy fuck that ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -72,30 +72,43 @@ export default function Home() {
       }
     }
   }, [editorText]);
-  
+
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <main className="flex min-h-screen flex-col items-center justify-between">
-        <nav className="w-screen flex justify-between items-center mb-4 p-1 h-[5vh]">
-          <div className="flex flex-row items-center justify-between w-full">
-            <h1 className="font-bold text-3xl">AI.DE</h1>
-            <div className="flex flex-row items-center space-x-4">
-              <p>For the next generation of resume builders and math proof makers.</p>
-              <ModeToggle />
-            </div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-32">
+        <nav className="w-full flex justify-between items-center mb-4">
+          <div className="flex">
+            <h1 className="font-bold text-9xl">AI.DE</h1>
+            <p className="pt-24 pl-5">
+              For the next generation of resume builders and math proof makers.
+            </p>
           </div>
+          <ModeToggle />
         </nav>
         <div className="flex items-center justify-between w-full">
           <div className="flex w-full h-full rounded-md overflow-hidden">
             {" "}
-            <MonacoEditor setEditorText={setEditorText} />
+            <MonacoEditor editorText = {editorText} setEditorText={setEditorText} />
           </div>
-          {!isLoading ?
-            <PDFViewer filePath={data} />
-            // <iframe src={data} width="100%" height="500px" style={{ border: 'none' }}></iframe>
-            : <div></div>}
-          <div className="flex w-full items-center justify-between py-10">
+            {!isLoading ?
+                <PDFViewer filePath={data} />
+              // <iframe src={data} width="100%" height="500px" style={{ border: 'none' }}></iframe>
+              : null}
+        </div>
+        <div className="flex w-full h-full items-center justify-between py-10">
+          <div className="flex flex-col">
+            <div className="z-1 bg-[#e9c7f2] rounded-3xl">
+              <h1 className="text-5xl font-bold z-2 py-2 px-2 text-[#a803d2]">
+                Discover
+              </h1>
+            </div>
+            <h2>See the commands we offer</h2>
             <ScrollBaby />
+          </div>
+          <div className="flex flex-col">
+            <h1>Search</h1>
+            <h2>Look for what piques your interest</h2>
           </div>
         </div>
       </main>
--- client/src/components/monaco-editor.tsx
+++ client/src/components/monaco-editor.tsx
@@ -322,8 +322,7 @@ const MonacoEditor = ({
 
   return (
     <Editor
-      height="95vh"
-      width="50vw"
+      className="w-full h-[792px] border-2 border-gray-300 rounded-md"
       defaultLanguage="latex"
       defaultValue={latexTemplate}
       theme={theme === "dark" ? "vs-dark" : "light"} // Use monaco's built-in themes as fallback
